1053fe2859153608f1d9aa891c629d7b6b2a3109
Elisa Falanga <falangae@elisas-air.bc.edu>
2022-11-27 18:28:09 -0500

    improved graphs

--- Module 7_CODE.R
+++ Module 7_CODE.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(ggplot2)
 library(MuMIn)
 
-ang <- seq(45,168.75,length.out = 12) #elbow angle
 f <- list.files("Project 8 data/",full.names=TRUE)
 dat.l <- list()
 
@@ -32,14 +31,18 @@ colnames(fmax) <- c("FMAX","who","angle_max","activity")
 normalized_table <- merge(max_value,fmax,by="who")%>%  
   filter(activity.x==activity.y)
 normalized_table$fnorm <- (normalized_table$X1/normalized_table$FMAX)
-fnorms <- c(normalized_table$X1/normalized_table$FMAX)
+
 
 df1 <- normalized_table%>%
   select(who,angle,activity.x,fnorm)
 df1$angle <- as.numeric(as.character(df1$angle))
-df1_fatigue <- df1[df1$activity.x=="fatigue",]
-df1_control <- df1[df1$activity.x=="control",]
 
+df1_fatigue <- df1[df1$activity.x=="fatigue",]
+fnorm_fatigue <- c(df1_fatigue$fnorm)
+ang.F <- df1_fatigue$angle
+df1_control <- df1[df1$activity.x=="control",]
+fnorm_control <- c(df1_control$fnorm)
+ang.C <- df1_control$angle
 
 poly.m2 <- lm(fnorm~poly(angle,2),df1_control) #second order
 poly.m3 <- lm(fnorm~poly(angle,3),df1_control) #third order
@@ -47,10 +50,27 @@ poly.m4 <- lm(fnorm~poly(angle,4),df1_control) #fourth order
 
 AICc(poly.m2,poly.m3,poly.m4) #the fourth model fits the best
 
+
 x.pred <- seq(45,157.5,length.out = 1000) #define 1000 angles from our range
 
 normF.pred <- predict(poly.m4,newdata = data.frame(angle=x.pred)) #predict the force using 1000 angles
 
-qplot(angle,fnorms)+geom_point(aes(x=x.pred,y=normF.pred),col="red")+geom_point(aes(x=x.pred[which.max(normF.pred)],y=normF.pred[which.max(normF.pred)]),size=5,col="blue")
+qplot(ang.C,fnorm_control)+geom_point(aes(x=x.pred,y=normF.pred),col="red")+geom_point(aes(x=x.pred[which.max(normF.pred)],y=normF.pred[which.max(normF.pred)]),size=5,col="blue")
                                                                                                   
 x.pred[which.max(normF.pred)]
+
+qplot(ang.F,fnorm_fatigue)+geom_point(aes(x=ang[which.max(fnorm_fatigue)],y=fnorm_fatigue[which.max(fnorm_fatigue)]),col="red",size=4)
+
+ang.F[which.max(fnorm_fatigue)]
+
+poly.m2.fat <- lm(fnorm~poly(angle,2),df1_fatigue) #second order
+poly.m3.fat <- lm(fnorm~poly(angle,3),df1_fatigue) #third order
+poly.m4.fat <- lm(fnorm~poly(angle,4),df1_fatigue) #fourth order
+
+AICc(poly.m2.fat,poly.m3.fat,poly.m4.fat) #the third order model fits best
+
+normF.pred.fat <- predict(poly.m3.fat,newdata = data.frame(angle=x.pred)) #predict the force using 1000 angles
+
+qplot(ang.F,fnorm_fatigue)+geom_point(aes(x=x.pred,y=normF.pred.fat),col="red")+geom_point(aes(x=x.pred[which.max(normF.pred.fat)],y=normF.pred.fat[which.max(normF.pred.fat)]),size=5,col="blue")
+
+x.pred[which.max(normF.pred.fat)]-x.pred[which.max(normF.pred)]
